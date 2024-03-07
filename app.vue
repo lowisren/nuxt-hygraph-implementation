5056a862efac09d3b144c94216c6171c5699c0d9
lowisren <lo.etheridge@hygraph.com>
2024-03-07 10:36:44 -0500

    working nuxt hygraph implementation

--- app.vue
+++ app.vue
@@ -1,8 +1,15 @@
+// file name: app.vue
+// Description: This is the main file for your Nuxt app. It fetches data from  Hygraph and displays it on the page.
 <template>
-  <div>
-    <h1>{{ pages.title }}</h1>
+  <div class="text-center">
+    <h1 class="text-6xl py-12 font-bold" v-for="page in pages" :key="page.slug">
+    {{ page.title }}</h1>
+    <p class="text-xl" v-for="page in pages" :key="page.slug">
+    {{ page.body.text }}</p>
+    <button class="bg-indigo-500  hover:bg-indigo-700 text-white font-bold py-2 px-4 my-6 rounded"><a href="https://hygraph.com/docs/implementations" target="_blank">Implementation Docs</a></button>
   </div>
 </template>
+
 
 <script setup>
 const {data} = await useFetch(
